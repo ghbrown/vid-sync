fee56b783b3f3732edc562df6b6698e8e8fda52f
Cyano-Hibiki <yuzhe828@uw.edu>
2024-11-02 18:19:17 -0500

    Created main application script for flask

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, jsonify
+import main
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process_urls', methods=['POST'])
+def process_data():
+    urls = request.get_json() 
+    video_sync_url = main.generate_playback(urls)
+    result = {"message": "test", "ViewSync URL": video_sync_url}
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
