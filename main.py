4a2b9466642a6598315d6fb1a0ac7fa6477a7202
Cyano-Hibiki <yuzhe828@uw.edu>
2024-11-02 19:10:36 -0500

    Added code to execute entire process

--- main.py
+++ main.py
@@ -2,5 +2,23 @@ import utils
 import processor
 
 def generate_playback(urls):
+    error_msg = ""
+
+    # Filter out invalid urls
+    (filtered_urls, invalid_urls) = utils.filter_urls(urls)
+
+    if not invalid_urls:
+        error_msg += "Some of the URLs are not valid. They have been removed from the list."
+
+    file_paths = utils.get_audio(filtered_urls)
+
+    timestapms = processor.resolve_lags(file_paths)
+
+    viewsync_url = utils.generate_url(urls, timestapms)
+
+
     
-    return "test"
+
+
+
+    return viewsync_url
