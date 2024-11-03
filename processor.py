cb4329b69c777b092e026b5af202e5cee9652433
Kunuki <69844470+Kunuki@users.noreply.github.com>
2024-11-02 21:49:30 -0500

    Combine approaches

--- processor.py
+++ processor.py
@@ -18,23 +18,25 @@ def save_merged_signal(signal_list, lags, fs, filename):
     
     sf.write(filename, combined_signal, fs)
 
-# def compute_lag_pair(signal1, signal2, fs):
-#     min_length = min(signal1.shape[0], signal2.shape[0])
-#     F1 = fft(signal1)[:min_length]
-#     F2 = fft(signal2)[:min_length]
-
-#     cross_spectrum = F1 * np.conj(F2)
-#     cross_spectrum /= np.abs(cross_spectrum)
-#     inverse_fft = ifft(cross_spectrum)
-#     shifted = fftshift(inverse_fft)
-#     delay_idx = np.argmax(np.abs(shifted))
-#     lag = delay_idx - min(len(signal1), min_length) // 2
-#     if lag > 0:
-#         return (lag/fs, 0)
-#     else:
-#         return (0, -1*lag/fs)
-    
 def compute_lag_pair(signal1, signal2, fs):
+    min_length = min(signal1.shape[0], signal2.shape[0])
+    F1 = fft(signal1)[:min_length]
+    F2 = fft(signal2)[:min_length]
+
+    cross_spectrum = F1 * np.conj(F2)
+    cross = correlate(signal1[:min_length], signal2[:min_length], mode='full')
+    cross_spectrum /= np.abs(cross_spectrum)
+    inverse_fft = ifft(cross_spectrum)
+    shifted = np.abs(fftshift(inverse_fft))
+    correlation = shifted + 0.01 * cross
+    delay_idx = np.argmax(correlation)
+    lag = delay_idx - min(len(signal1), min_length) // 2
+    if lag > 0:
+        return (lag/fs, 0)
+    else:
+        return (0, -1*lag/fs)
+    
+def compute_lag_pair_cor(signal1, signal2, fs):
     corr = correlate(signal1, signal2, mode='full')
     lags = correlation_lags(signal1.shape[0], signal2.shape[0], mode="full")
     lag = lags[np.argmax(corr)]
@@ -56,17 +58,25 @@ def compute_lags(signal_list, fs):
     lags = [lag - m for lag in lags]
     return lags
 
-def resolve_lags(filelist):
-    fs = 8000
+def resolve_lags(filelist, fs):
     signal_list = []
     for filename in filelist:
         signal, _ = librosa.load(filename, sr=fs, mono=True)
-        # harmonic, percussive = librosa.effects.hpss(signal)
         signal_list.append(signal)
     lags = compute_lags(signal_list, fs)
     return lags
 
+def resolve_lags_cor(filelist, fs):
+    signal_list = []
+    for filename in filelist:
+        signal, _ = librosa.load(filename, sr=fs, mono=True)
+        signal_list.append(signal)
+    lags = compute_lag_pair(signal_list, fs)
+    return lags
+    
+
 if __name__ == "__main__":
+    # signal_name = ["7MtuoPeC4tE.mp3", "8B2Je1pZVpo.mp3", "ej85EfL1HYU.mp3", "HKZ0_UQvaOw.mp3"]
     signal_name = ["MOCOCO.mp3", "FUWAWA.mp3"]
     fs = 8000
     
@@ -74,9 +84,7 @@ if __name__ == "__main__":
     for filename in signal_name:
         signal, _ = librosa.load(filename, sr=fs, mono=True)
         harmonic, percussive = librosa.effects.hpss(signal)
-        # save_merged_signal([harmonic], [0], fs, filename.split('.')[0]+"_harmonic.mp3")
-        # save_merged_signal([percussive], [0], fs, filename.split('.')[0]+"_percussive.mp3")
-        signal_list.append(percussive)
+        signal_list.append(signal)
 
     lags = resolve_lags(signal_name)
     for name, lag, in zip(signal_name, lags):
